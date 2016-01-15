cf29dfaf27c26abdc5d7729ba2485830c9986dfb
viky <vikas.yadav@iiitb.org>
2016-01-15 23:33:12 +0530

    Added relationships between models. Modified volunteers table code. Added some helper functions for admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,6 @@ class User < ActiveRecord::Base
 	validates :phone_number,:presence => true,
                  :numericality => true,
                  :length => { :minimum => 10, :maximum => 10 }
+
+  has_one :volunteer_request # One user can make only one volunteer request
 end
--- app/models/volunteer_request.rb
+++ app/models/volunteer_request.rb
@@ -1,4 +1,7 @@
 class VolunteerRequest < ActiveRecord::Base
 	validates :user_id, presence: true, uniqueness: true
 	validates :tshirt_size, inclusion: { in: [1,2,3,4,5] }
+
+  belongs_to :user
+  belongs_to :event
 end
