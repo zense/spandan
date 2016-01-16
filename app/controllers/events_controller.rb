e3493c3c4054bc548a2e8abfe2492a756d8ba7ad
krishnathegreat <krishna.sharma.1996@gmail.com>
2016-01-16 16:32:23 +0530

    event_modified

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,45 +10,114 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.json
   def show
+    @user=User.all
   end
 
   # GET /events/new
   def new
+    @user=User.all
     @event = Event.new
   end
 
   # GET /events/1/edit
   def edit
+    @user=User.all
   end
 
   # POST /events
   # POST /events.json
+  def spoc_handle(email,count)
+    if User.exists?(:email=>email)
+        @user=User.find_by email:email
+        if @user.spoc==true
+          @eve=Event.find_by id:@user.spoc_event_id
+          @err[count]=email.to_s+" is already a spoc of the event \"".to_s+@eve.name+"\" click the edit button to enter another spoc"
+        else
+          @user.spoc_event_id=@event.id
+          @user.spoc=true
+          @user.save
+        end
+    else
+        @err[count]="The email \"".to_s+email+"\" was not found click on the edit button to enter a valid email"
+    end
+  end
+  def spoc
+    @err = Array.new(6, "")
+    spoc_handle(params["email_spoc1"],0)
+    spoc_handle(params["email_spoc2"],1)
+    spoc_handle(params["email_spoc3"],2)
+    spoc_handle(params["email_spoc4"],3)
+    spoc_handle(params["email_spoc5"],4)
+    spoc_handle(params["email_spoc6"],5)
+    if(params["email_spoc1"]=="")
+      @err[0]=""
+    end
+    if(params["email_spoc2"]=="")
+      @err[1]=""
+    end
+    if(params["email_spoc3"]=="")
+      @err[2]=""
+    end
+    if(params["email_spoc4"]=="")
+      @err[3]=""
+    end
+    if(params["email_spoc5"]=="")
+      @err[4]=""
+    end
+    if(params["email_spoc6"]=="")
+      @err[5]=""
+    end
+    @err1=@err[0]
+    @err2=@err[1]
+    @err3=@err[2]
+    @err4=@err[3]
+    @err5=@err[4]
+    @err6=@err[5]
+  end
   def create
     @event = Event.new(event_params)
 
-    respond_to do |format|
-      if @event.save
+
+    #render plain: @exist
+    #render plain: @user
+    if @event.save
+      spoc()
+      render "show"
+    else
+      render 'new'
+
+    #respond_to do |format|
+=begin
+       if @event.save
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
+       format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new }
         format.json { render json: @event.errors, status: :unprocessable_entity }
       end
+=end
     end
   end
 
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
   def update
-    respond_to do |format|
+    #respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
+        User.where(spoc_event_id: @event.id).find_each do |user|
+          user.spoc=false
+          user.spoc_event_id=0
+          user.save
+        end
+        spoc()
+        #format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        #format.json { render :show, status: :ok, location: @event }
+        render 'show'
       else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+        render 'edit'
+        #format.html { render :edit }
+        #format.json { render json: @event.errors, status: :unprocessable_entity }
       end
-    end
   end
 
   # DELETE /events/1
