c2fd5902db2b604dd2cda1a97b55ec31b2524e31
viky <vikas.yadav@iiitb.org>
2016-02-14 15:17:18 +0530

    MOdified and fixed errors on profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,18 +5,9 @@ class ProfilesController < ApplicationController
 	end
 
 	def show
-		@current=current_user
-		if @current.id != params[:id].to_i
-			render(:template => 'profiles/error')
-		else
-			@user=User.find(params[:id])
-			@volunteer=VolunteerRequest.find_by_user_id(@user.id)
-			@event1=Event.find_by_id(@volunteer.priority1)
-			@event2=Event.find_by_id(@volunteer.priority2)
-			@event3=Event.find_by_id(@volunteer.priority3)
-			@event4=Event.find_by_id(@volunteer.priority4)
+		@user=User.find(params[:id])
+		if @user.nil?
+			render_404
 		end
-		
 	end
-
 end
