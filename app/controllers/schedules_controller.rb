efeef752a861e062e9d38adb0442a5b0a2484bc9
viky <vikas.yadav@iiitb.org>
2016-03-02 11:53:26 +0530

    Schedules integrated

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,6 @@
 class SchedulesController < ApplicationController
   before_action :set_schedule, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /schedules
   # GET /schedules.json
   def index
