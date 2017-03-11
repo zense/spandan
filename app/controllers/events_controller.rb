b25090952236f553782d01e5a600216b0bc755c8
PranavAggarwal <pranavaggarwal5@gmail.com>
2017-03-11 13:32:54 +0530

    Fixed events on home, remove authentication, hid sign out and login option

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   # Make this controller available only to admins
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   # GET /events
